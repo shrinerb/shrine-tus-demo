1579232e8b4e0540c469d4585ff82da50aeec0f6
Janko Marohnić <janko.marohnic@gmail.com>
2017-12-24 13:27:06 +0100

    Use Uppy as a wrapper around tus-js-client
    Uppy is probably the best file upload library out there now, as it's
    extremely flexible, and it supports both resumable and regular uploads
    (the Tus plugin uses tus-js-client internally), which makes it more
    universal.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,39 @@
-Object.assign(tus.defaultOptions, {
-  endpoint: "http://localhost:9000/",
-  retryDelays: [0, 1000, 3000, 6000, 10000],
-});
+var fileInput = document.querySelector('input[type=file]')
+var wrapper   = fileInput.parentNode
 
-document.querySelectorAll("input[type=file]").forEach(function(fileInput) {
-  fileInput.addEventListener("change", function() {
-    for (var i = 0; i < fileInput.files.length; i++) {
-      var file = fileInput.files[i],
-          progressBar = document.querySelector(".progress").cloneNode(true);
+uppy = Uppy.Core({ id: 'tus' })
+  .use(Uppy.FileInput, {
+    target:               wrapper,
+    allowMultipleFiles:   false,
+    replaceTargetContent: true
+  })
+  .use(Uppy.Tus, {
+    endpoint: 'http://localhost:9000/'
+  })
+  .use(Uppy.ProgressBar, {
+    target: wrapper
+  })
 
-      fileInput.parentNode.insertBefore(progressBar, fileInput);
+uppy.run()
 
-      var upload = new tus.Upload(file, {
-        metadata: {
-          "filename":     file.name.match(/[^\/\\]+$/)[0], // for "Content-Disposition"
-          "content_type": file.type,                       // for "Content-Type"
-        },
-      });
+uppy.on('upload-success', function(fileId, data) {
+  var file = uppy.getFile(fileId)
 
-      upload.options.onProgress = function(bytesSent, bytesTotal) {
-        var progress = parseInt(bytesSent / bytesTotal * 100, 10);
-        var percentage = progress.toString() + '%';
-        progressBar.querySelector(".progress-bar").style = "width: " + percentage;
-        progressBar.querySelector(".progress-bar").innerHTML = percentage;
-      };
-
-      upload.options.onSuccess = function(result) {
-        fileInput.parentNode.removeChild(progressBar);
-
-        // custruct uploaded file data in the Shrine attachment format
-        var fileData = {
-          id: upload.url,
-          storage: "cache",
-          metadata: {
-            filename:  file.name.match(/[^\/\\]+$/)[0], // IE returns full path
-            size:      file.size,
-            mime_type: file.type,
-          }
-        };
-
-        // assign file data to the hidden field so that it's submitted to the app
-        var hiddenInput = fileInput.parentNode.querySelector("input[type=hidden]");
-        hiddenInput.value = JSON.stringify(fileData);
-
-        urlElement = document.createElement("p");
-        urlElement.innerHTML = upload.url;
-        fileInput.parentNode.insertBefore(urlElement, fileInput.nextSibling);
-      };
-
-      upload.options.onError = function(error) {
-        if (error.originalRequest.status == 0) { // no internet connection
-          setTimeout(function() { upload.start() }, 5000);
-        }
-        else {
-          alert(error);
-        }
-      };
-
-      // start the tus upload
-      upload.start();
-    };
-
-    // remove selected files
-    fileInput.value = "";
+  var uploadedFileData = JSON.stringify({
+    id: data.url,
+    storage: "cache",
+    metadata: {
+      filename:  file.name,
+      size:      file.size,
+      mime_type: file.type,
+    }
   });
-});
+
+
+  var hiddenInput = wrapper.parentNode.querySelector("input[type=hidden]");
+  hiddenInput.value = uploadedFileData;
+
+  var videoLink = wrapper.parentNode.querySelector('.video-preview a')
+  videoLink.href = data.url
+  videoLink.innerHTML = data.url
+})
