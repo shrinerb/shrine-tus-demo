12a0f0e8503cc3787c273181c735f910483bb2cd
Janko Marohnić <janko.marohnic@gmail.com>
2017-12-30 23:21:02 +0100

    Minor improvements to the JS implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,44 @@
-var fileInput = document.querySelector('input[type=file]')
-var wrapper   = fileInput.parentNode
+// This code uses:
+//
+// * babel-polyfill (https://babeljs.io/docs/usage/polyfill/)
+// * tus-js-client (https://github.com/tus/tus-js-client)
+// * uppy (https://uppy.io)
 
-uppy = Uppy.Core({ id: 'tus' })
-  .use(Uppy.FileInput, {
-    target:               wrapper,
-    allowMultipleFiles:   false,
-    replaceTargetContent: true
+document.querySelectorAll('input[type=file]').forEach(function (fileInput) {
+  fileInput.style.display = 'none' // uppy will add its own file input
+
+  uppy = Uppy.Core({ id: fileInput.id })
+    .use(Uppy.FileInput, {
+      target:             fileInput.parentNode,
+      allowMultipleFiles: fileInput.multiple
+    })
+    .use(Uppy.Tus, {
+      endpoint: 'http://localhost:9000/'
+    })
+    .use(Uppy.ProgressBar, {
+      target: fileInput.parentNode
+    })
+
+  uppy.run()
+
+  uppy.on('upload-success', function(fileId, data) {
+    var file = uppy.getFile(fileId)
+
+    var uploadedFileData = JSON.stringify({
+      id: data.url,
+      storage: "cache",
+      metadata: {
+        filename:  file.name,
+        size:      file.size,
+        mime_type: file.type,
+      }
+    })
+
+    var hiddenInput = document.getElementById(fileInput.dataset.uploadResultElement)
+    hiddenInput.value = uploadedFileData;
+
+    var videoLink = document.getElementById(fileInput.dataset.previewElement)
+    videoLink.href = data.url
+    videoLink.innerHTML = data.url
   })
-  .use(Uppy.Tus, {
-    endpoint: 'http://localhost:9000/'
-  })
-  .use(Uppy.ProgressBar, {
-    target: wrapper
-  })
-
-uppy.run()
-
-uppy.on('upload-success', function(fileId, data) {
-  var file = uppy.getFile(fileId)
-
-  var uploadedFileData = JSON.stringify({
-    id: data.url,
-    storage: "cache",
-    metadata: {
-      filename:  file.name,
-      size:      file.size,
-      mime_type: file.type,
-    }
-  });
-
-
-  var hiddenInput = wrapper.parentNode.querySelector("input[type=hidden]");
-  hiddenInput.value = uploadedFileData;
-
-  var videoLink = wrapper.parentNode.querySelector('.video-preview a')
-  videoLink.href = data.url
-  videoLink.innerHTML = data.url
 })
