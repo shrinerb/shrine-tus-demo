5278e024ff796d04aa8f18dbd94c980b3277de66
Janko Marohnić <janko.marohnic@gmail.com>
2019-01-26 21:22:17 +0100

    Make it work with Falcon's default SSL server
    Falcon runs the server on https://localhost:9292 by default, using a
    self-signed certificate. In order for Shrine::Storage::Tus to be able to
    download the uploaded tus file during Shrine promotion, its downloader
    needs to be configured to use Falcon's self-signed certificate. We can
    fetch it with the localhost gem.

--- config/shrine.rb
+++ config/shrine.rb
@@ -1,11 +1,16 @@
 require "shrine"
 require "shrine/storage/file_system"
 require "shrine/storage/tus"
+require "down/http"
+require "localhost/authority"
+
+# use Falcon's self-signed SSL certificate for downloading tus files from localhost
+downloader = Down::Http.new(ssl_context: Localhost::Authority.fetch.client_context)
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),
-  tus:   Shrine::Storage::Tus.new,
+  tus:   Shrine::Storage::Tus.new(downloader: downloader),
 }
 
 Shrine.plugin :sequel
